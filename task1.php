905655f81a07e8874c7e8fd7d5299e5daa7bce05
Шамиль Нурмагомедов <snurmagomedov@kit-consulting.ru>
2022-06-29 20:33:56 +0300

    Fix tast1

--- task1.php
+++ task1.php
@@ -5,17 +5,18 @@ class Solution
     public function printLadder(int $from = 0, int $to = 1000): void
     {
         $currentRowNumber = 1;
-        $currentRow = [];
-        $padLength = strlen($to);
+        $numbersInCurrentRow = 0;
+        $padLength = strlen($to) + 1;
 
         for ($i = $from; $i <= $to; $i++) {
-            $currentRow[] = str_pad($i, $padLength, ' ', STR_PAD_LEFT);
+            echo str_pad($i, $padLength, ' ', STR_PAD_LEFT);
 
-            if (count($currentRow) == $currentRowNumber) {
-                echo implode(' ', $currentRow) . "\n";
+            $numbersInCurrentRow += 1;
 
-                $currentRow = [];
+            if ($numbersInCurrentRow == $currentRowNumber || $i == $to) {
+                $numbersInCurrentRow = 0;
                 $currentRowNumber += 1;
+                echo "\n";
             }
         }
     }
